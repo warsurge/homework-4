c146bf7799a0d9ba79aed3058f5cd3687ac40a65
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-04-25 14:00:15 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # homework-4
-Coding quiz
+Timed quiz
+
+# User Story
+AS A coding bootcamp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+
+# Acceptance Criteria
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+
+# What I Did
+* I first started psuedo coding out what would be the basic blocks for the code.
+* I then just started trying to get the start button on screen.
+* I then moved on to working with my group to get the quiz to start.
+* After getting the questions to load, I went on to getting the quiz to recognize what was right and what was wrong.
+* Finally I could start with a timer and setting up on the screen.
+* I then had the countdown timer started and was able to get the subtraction go from the wrong answers.
+* Thanks to help from our study group I was able to get the end game screen to load correctly.
+* I then used Bootstrap and tried to design it more with some custom css for a unique look.
+* Unfortunetly I ran out of time and wasn't able to figure out the local storage, and how to get the names to stick.
+* 
---
+++ questions.js
@@ -0,0 +1,64 @@
+const theQuestions =
+  [
+    {
+      question: "What is Batman's real identity?",
+      answers: [ 
+        { text: "Clark Kent", correct: false },
+        { text: "Wally West", correct: false },
+        { text: "Mark Hamill", correct: false },
+        { text: "Bruce Wayne", correct: true }
+      ]
+    },
+    
+    {
+      question: "What is the name of Batman's Sidekick?",
+      answers: [
+        { text: "Wonder Boy", correct: false },
+        { text: "Blue Bird", correct: false },
+        { text: "The Yellow Hornet", correct: false },
+        { text: "Robin", correct: true }
+      ]
+    },
+    
+    {
+      question: "What is the name of Batman's car?",
+      answers: [ 
+        { text: "The Bat Car", correctcorrect: false },
+        { text: "The Batmobile", correct: true },
+        { text: "The Scooter", correctcorrect: false },
+        { text: "ZOOM ZOOM", correctcorrect: false }
+      ]
+    },
+    
+    {
+      question: "What is the name of Batman's butler?",
+      answers: [
+        { text: "Steve", correct: false },
+        { text: "John", correct: false },
+        { text: "Albert", correct: false },
+        { text: "Alfred", correct: true }
+      ]
+    },
+    
+    {
+      question: "Which Batman villan would love this quiz?",
+      answers: [ 
+        { text: "Riddler", correct: true },
+        { text: "Joker", correct: false },
+        { text: "Penguin", correct: false },
+        { text: "Two-Face", correct: false }
+      ]
+    },
+    
+    {
+      question: "What is the name of the actor who voiced the Joker in Batman the animated series?",
+      answers: [
+        { text: "Bruce Wayne", correct: false },
+        { text: "Adam West", correct: false },
+        { text: "Christian Bale", correct: false },
+        { text: "Mark Hamill", correct: true }
+      ]
+    },
+  ]
+
+
